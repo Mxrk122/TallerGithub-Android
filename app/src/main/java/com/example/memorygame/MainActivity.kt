cb2fea6627980b2202a2a53d0333a24b4094c364
Marco <m4rkg12@gmail.com>
2021-04-09 14:39:13 -0600

    Primer commit

---
+++ app/src/main/java/com/example/memorygame/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.memorygame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    // Variables que contendran objetos de la app
+
+    private lateinit var rvBoard: RecyclerView
+    private lateinit var MovesText: TextView
+    private lateinit var PairText: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Otorgando objeto
+        rvBoard = findViewById(R.id.rvBoard)
+        MovesText = findViewById(R.id.MovesText)
+        PairText = findViewById(R.id.PairText)
+
+        // Ordenando las cosas
+
+        // Crear adaptador para ordenar el RecyclerView
+        rvBoard.adapter = MemoryBoardAdapter(this, 8)
+        rvBoard.setHasFixedSize(true)
+        rvBoard.layoutManager = GridLayoutManager(this,2)
+    }
+}
---
+++ app/src/main/java/com/example/memorygame/MemoryBoardAdapter.kt
@@ -0,0 +1,33 @@
+package com.example.memorygame
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class MemoryBoardAdapter(private val context: Context, private val numPieces: Int) : // La clase adaptador es asbtracta, estamos haciendo un hijo del adaptador de rvBoard
+    RecyclerView.Adapter<MemoryBoardAdapter.ViewHolder>() { // Le pasamos como parametro al adaptador nuestra clase viewholder
+
+
+    // Metodos de la clase abstracta
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.memory_card, parent, false)
+        // Devolver la vista envuelta dentro de un soporte de vista
+        return ViewHolder(view)
+    }
+
+    // Este metodo nos pregunta ¿Cual es la cantidad de elementos que hay en nuestro RecyclerView?
+    override fun getItemCount() = numPieces
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(position)
+    }
+
+    // declarando una clase de viewholder
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(position: Int) {
+            // No-op aun
+        }
+    }
+}
